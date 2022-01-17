9d46743d35b4a56806bf24e2d4c6fd61d2a8cdbc
Eva-Renata <all2evi@yahoo.com>
2022-01-17 17:33:08 +0100

    first commit

---
+++ Sites/codealong_app/src/Components/Pages/About/About.jsx
@@ -0,0 +1,9 @@
+export const About = () => {
+	return (
+		<>
+		<h1>Om os</h1>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quis deleniti architecto nam cupiditate quaerat, aut eos, neque eaque voluptate consectetur blanditiis dolores! Aperiam molestiae quia aut asperiores et nulla.</p>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quis deleniti architecto nam cupiditate quaerat, aut eos, neque eaque voluptate consectetur blanditiis dolores! Aperiam molestiae quia aut asperiores et nulla.</p>
+		</>
+	)
+}
